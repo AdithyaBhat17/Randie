8bd9b55e1dfe9cf2c4cffd6aa1fd0eeba2b3396a
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-09-02 19:17:58 +0530

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const generatePassword = require('password-generator')
+const path = require('path')
+
+const app = express()
+
+//serving static files via react.
+app.use(express.static(path.join(__dirname,'client/build')))
+
+//api endpoints
+app.get('/api/passwords',(req,res)=>{
+    const count = 5
+    const passwords = Array.from(Array(count).keys()).map(index => generatePassword(12,false))
+    res.json(passwords)
+})
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname+'client/build/index.html'));
+})
+
+const port = process.env.PORT || 5000
+app.listen(port)
+
+console.log(`Password generator listening on ${port}`)
