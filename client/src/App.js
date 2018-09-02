8a10d16ac7d7a9b555d1fc633a9d738c96778b4b
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-09-02 20:28:39 +0530

    final commit

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      passwords:[]
+    }
+  }
+
+  componentDidMount(){
+    this.getPasswords()
+  }
+
+  getPasswords = () => {
+    fetch('/api/passwords').then(res => res.json()).then(passwords => this.setState({passwords}))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.passwords ? (
+            <div>
+              <h1>Randie</h1>
+              <p>A random password generator <br/> <span>ʕʘ̅͜ʘ̅ʔ</span>  </p>
+              <ul>
+                {this.state.passwords.map((password,index) => 
+                  <li key={index}>
+                    {password}
+                  </li>
+                )}  
+              </ul> 
+              <button 
+              className="more" 
+              onClick={()=>this.getPasswords()}>
+                Too ez?
+              </button> 
+            </div>
+          ) : (
+            <div>
+              <h1>Ahhh!! I "ran" out of passwords :(</h1>
+              <button
+              className="more"
+              onClick={this.getPasswords()}>
+                Aww try again boii
+              </button>
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+export default App
